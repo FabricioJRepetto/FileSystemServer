7ded7e3e42cc34c73d00da9f4e01982ee1a6536a
Fabricio Repetto <fabricio.repetto@ncratleos.com>
2025-08-25 13:30:56 -0300

    elimino archivos extra

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"os/exec"
+	"time"
 )
 
 func main() {
@@ -21,5 +26,165 @@ func main() {
 	err := http.ListenAndServe(port, nil)
 	if err != nil {
 		logMensaje("ERROR", "Error al iniciar el servidor:"+err.Error())
+	}
+}
+
+type FileRequest struct {
+	OldName    string `json:"oldName"`
+	NewName    string `json:"newName"`
+	DeleteFile string `json:"deleteFile"`
+	MoveFile   bool   `json:"moveFile"`
+}
+
+// Copia, renombra, mueve y elimina archivos de imagenes de cheques.
+func manageCheckFilesHandler(w http.ResponseWriter, r *http.Request) {
+	ORIGIN_PATH := "C:\\ncr-cc\\temp\\ipm\\"
+	FILES_PATH := "C:\\ncr-cc\\temp\\checks-images\\"
+
+	createDirs(w, FILES_PATH)
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
+		logMensaje("ERROR", "Método no permitido: "+r.Method)
+		return
+	}
+
+	// Decodificar el JSON del body
+	var actions []FileRequest
+	err := json.NewDecoder(r.Body).Decode(&actions)
+	if err != nil {
+		http.Error(w, "Error en el body", http.StatusBadRequest)
+		logMensaje("ERROR", "Error en el body: "+err.Error())
+		return
+	}
+
+	for _, action := range actions {
+		// Mover el archivo .tif renombrado
+		if action.OldName != "" && action.NewName != "" {
+			err := os.Rename(ORIGIN_PATH+action.OldName, FILES_PATH+action.NewName)
+			if err != nil {
+				http.Error(w, "Error al renombrar: "+err.Error(), http.StatusInternalServerError)
+				logMensaje("ERROR", "Error al renombrar: "+err.Error())
+				return
+			} else {
+				logMensaje("STATUS", "Archivo renombrado: "+action.OldName+" a "+action.NewName)
+			}
+		}
+
+		// Eliminar archivo .jpg
+		if action.DeleteFile != "" {
+			err := os.Remove(ORIGIN_PATH + action.DeleteFile)
+			if err != nil {
+				http.Error(w, "Error al eliminar: "+err.Error(), http.StatusInternalServerError)
+				logMensaje("ERROR", "Error al eliminar: "+err.Error())
+				return
+			} else {
+				logMensaje("STATUS", "Archivo eliminado: "+action.DeleteFile)
+			}
+		}
+
+		// Mover archivo a recurso compartido
+		if action.MoveFile == true && action.NewName != "" {
+			moveToSharedFolder(FILES_PATH + action.NewName)
+		}
+	}
+
+	// Desmontar carpeta de red al finalizar proceso
+	desmontarCarpetaRed()
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, `{"status":"Archivos procesados correctamente"}`)
+	logMensaje("OK", "✅ Archivos procesados correctamente.")
+}
+
+func logMensaje(logType string, mensaje string) {
+	logPath := "C:\\ncr-cc\\logs\\file-system-server.log"
+	f, err := os.OpenFile(logPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Error al abrir el archivo de log:", err)
+		return
+	}
+	defer f.Close()
+
+	timestamp := time.Now().Format("2006-01-02 15:04:05")
+	logEntry := fmt.Sprintf("%s [%s] %s\n", timestamp, logType, mensaje)
+	fmt.Println(logEntry)
+	f.WriteString(logEntry)
+}
+
+// Crea los directorios si no existen
+func createDirs(w http.ResponseWriter, paths ...string) {
+	for _, path := range paths {
+		if _, err := os.Stat(path); os.IsNotExist(err) {
+			err = os.MkdirAll(path, 0755)
+			if err != nil {
+				http.Error(w, "Error al crear directorio: "+err.Error(), http.StatusInternalServerError)
+				logMensaje("ERROR", "Error al crear directorio: "+path+" - "+err.Error())
+			} else {
+				logMensaje("STATUS", "Directorio creado: "+path)
+			}
+		}
+	}
+
+	// Configuración carpeta de red
+	remoteShare := `\\10.241.162.33\tfrfile\clearing\imagenesTAS`
+	user := `GSCORP.AD\svcTASimag`
+	password := `DVX4j32YGkobg0xkSNw4aA==`
+
+	// Montar carpeta de red
+	cmdMap := exec.Command("cmd", "/C", "net", "use", "Z:", remoteShare, "/user:"+user, password)
+	errMount := cmdMap.Run()
+	if errMount != nil {
+		logMensaje("ERROR", "Error al montar la carpeta de red: "+errMount.Error())
+		return
+	} else {
+		logMensaje("STATUS", "Carpeta de red montada correctamente.")
+	}
+}
+
+// Función para copiar archivo
+func copyFile(src, dst string) error {
+	sourceFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer sourceFile.Close()
+
+	destFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer destFile.Close()
+
+	_, err = io.Copy(destFile, sourceFile)
+	if err != nil {
+		return err
+	}
+
+	return destFile.Sync()
+}
+
+// Función para mover archivo a recurso compartido
+func moveToSharedFolder(filePath string) {
+	// Mover archivo
+	cdmMove := exec.Command("cmd", "/C", "move", filePath, "Z:\\")
+	err := cdmMove.Run()
+	if err != nil {
+		logMensaje("ERROR", "Error al mover: "+err.Error())
+	} else {
+		logMensaje("STATUS", "Archivo movido a recurso compartido")
+	}
+}
+
+// Desmontar carpeta de red al finalizar proceso
+func desmontarCarpetaRed() {
+	// Desconectar carpeta de red
+	cmdDel := exec.Command("cmd", "/C", "net", "use", "Z:", "/delete", "/y")
+	errDel := cmdDel.Run()
+	if errDel != nil {
+		logMensaje("ERROR", "Error al desmontar la unidad: "+errDel.Error())
+	} else {
+		logMensaje("STATUS", "Unidad desmontada correctamente.")
 	}
 }
